4c845cdaed52be17e88e9662309b825bff4f3359
Max Hng <maxkhuan1@gmail.com>
2024-06-11 10:31:15 +0800

    add s

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ mongoose.connect(process.env.MONGODB_URI|| 'mongodb://127.0.0.1:27017/smpp-clien
     // useNewUrlParser: true,
     // useUnifiedTopology: true
 }).then(() => {
-    logger.info('Connected to MongoDBs');
+    logger.info('Connected to MongoDBssssss');
 }).catch((err) => {
     logger.error('Failed to connect to MongoDB', err);
 });
