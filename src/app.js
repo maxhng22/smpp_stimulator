d40664fbf46c452fd503f76a2089f7c8b949ce3d
terence818 <terencetee818@gmail.com>
2024-06-12 10:48:03 +0800

    fix jsontoken

--- src/app.js
+++ src/app.js
@@ -1,17 +1,17 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const config = require('./utils/config');
-const userController = require('./controllers/userController');
-const messageController = require('./controllers/messageController');
-const configController = require('./controllers/configController');
-const smppService = require('./services/smppService');
-const logger = require('./utils/logger');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const config = require("./utils/config");
+const userController = require("./controllers/userController");
+const messageController = require("./controllers/messageController");
+const configController = require("./controllers/configController");
+const smppService = require("./services/smppService");
+const logger = require("./utils/logger");
 const app = express();
-require('dotenv').config();
+require("dotenv").config();
 
-const JWT_SECRET="your key";
+const jwt = require("jsonwebtoken");
 
 // require('dotenv').config()
 
@@ -20,43 +20,51 @@ app.use(cors());
 
 // Connect to MongoDB
 
-mongoose.connect(process.env.MONGODB_URI|| 'mongodb://127.0.0.1:27017/smpp-client', {
+mongoose
+  .connect(process.env.MONGODB_URI, {
     // useNewUrlParser: true,
     // useUnifiedTopology: true
-}).then(() => {
-    logger.info('Connected to MongoDBssssss');
-}).catch((err) => {
-    logger.error('Failed to connect to MongoDB', err);
-});
+  })
+  .then(() => {
+    logger.info("Connected to MongoDBssssss");
+  })
+  .catch((err) => {
+    logger.error("Failed to connect to MongoDB", err);
+  });
 
 // Middleware to authenticate JWT token
 const authenticate = (req, res, next) => {
+  const token = req.header("Authorization");
+  if (!token) {
+    console.log("token", token);
+    return res.status(401).json({ message: "Access denied" });
+  }
+  try {
+    const decoded = jwt.verify(
+      token.replace("Bearer ", ""),
+      process.env.JWT_SECRET
+    );
 
-    console.log(req.header('Authorization'))
-    const token = "req.header('Authorization')";
-    if (!token) {
-        console.log("token", token)
-        return res.status(401).json({ message: 'Access denied' });
-    }
-    try {
-        console.log("token", token)
-        const decoded = jwt.verify(token.replace('Bearer ', ''), process.env.JWT_SECRET);
-        req.user = decoded;
-        next();
-    } catch (err) {
-        res.status(400).json({ message: 'Invalid token' });
-    }
+    req.user = decoded;
+    next();
+  } catch (err) {
+    res.status(400).json({ message: "Invalid token" });
+  }
 };
 
-app.post('/api/register', userController.register);
-app.post('/api/login', userController.login);
-app.post('/api/saveconfig', authenticate, configController.saveConfig);
-app.get('/api/getconfig', authenticate, configController.getConfig);
-app.post('/api/send-message', authenticate, messageController.sendMessage);
-app.get('/api/smppConnection', authenticate, messageController.getSMPPConnectionStatus);
-app.post('/api/submit-message', authenticate, messageController.submitMessage);
+app.post("/api/register", userController.register);
+app.post("/api/login", userController.login);
+app.post("/api/saveconfig", authenticate, configController.saveConfig);
+app.get("/api/getconfig", authenticate, configController.getConfig);
+app.post("/api/send-message", authenticate, messageController.sendMessage);
+app.get(
+  "/api/smppConnection",
+  authenticate,
+  messageController.getSMPPConnectionStatus
+);
+app.post("/api/submit-message", authenticate, messageController.submitMessage);
 
 const PORT = process.env.PORT || 3004;
 app.listen(PORT, () => {
-    logger.info(`Server is running on port ${PORT}`);
+  logger.info(`Server is running on port ${PORT}`);
 });
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -50,7 +50,7 @@ exports.login = async (req, res) => {
         }
 
         // Generate a JWT token
-        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET || 'defaultSecretKey', { expiresIn: '1h' });
+        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET , { expiresIn: '1h' });
 
         res.status(200).json({ token });
     } catch (error) {
